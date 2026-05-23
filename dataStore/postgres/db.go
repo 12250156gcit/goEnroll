2447436b2d36363c210c31ce1a0bea724a2ab12b
12250156_GitUser <12250156.gcit@rub.edu.bt>
2026-05-23 13:58:05 +0000

    updated render db info

--- dataStore/postgres/db.go
+++ dataStore/postgres/db.go
@@ -8,20 +8,22 @@ import (
 	_ "github.com/lib/pq"
 )
 
-//db details
+// db details
 const (
-	postgres_host = "db"
-	postgres_port = 5432
-	postgres_user = "postgres"
-	postgres_password = "postgres"
-	postgres_dbname = "my_db"
+	postgres_host     = "dpg-d88qcaugvqtc73bci18g-a.singapore-postgres.render.com"
+	postgres_port     = 5432
+	postgres_user     = "postgres_admin"
+	postgres_password = "bWjvBsTMtraa96Zd1bk5aktOKjtGLTpx"
+	postgres_dbname   = "my_db_3k99"
 )
-//create pointer variable Db which points to sql driver
+
+// create pointer variable Db which points to sql driver
 var Db *sql.DB
-//init() is always called before main() by the Go compiler
+
+// init() is always called before main() by the Go compiler
 func init() {
 	//creating a database connection string
-	db_info := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", postgres_host, postgres_port, postgres_user, postgres_password, postgres_dbname)
+	db_info := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s", postgres_host, postgres_port, postgres_user, postgres_password, postgres_dbname)
 
 	var err error
 
